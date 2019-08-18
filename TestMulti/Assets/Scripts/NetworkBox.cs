6919ee65e4561eda9c6deba0c9d73c1c11bc2ce8
sheido <af8081@gmail.com>
2019-08-19 00:18:26 +0200

    box fix

--- TestMulti/Assets/Scripts/NetworkBox.cs
+++ TestMulti/Assets/Scripts/NetworkBox.cs
@@ -7,15 +7,6 @@ public class NetworkBox : Photon.MonoBehaviour
     private Vector3 _realPosition;
     private Quaternion _realQuaternion;
 
-    void Update()
-    {
-        if (!photonView.isMine)
-        {
-            transform.position = Vector3.Lerp(transform.position, _realPosition, 0.1f);
-            transform.rotation = Quaternion.Lerp(transform.rotation, _realQuaternion, 0.1f);
-        }
-    }
-
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         if (stream.isWriting)
